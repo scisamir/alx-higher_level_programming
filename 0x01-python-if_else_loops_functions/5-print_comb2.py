7cb2885b4cdc2ca95a6c63f56f9dd85b835f9c1e
Samir Idris <samzidris11@gmail.com>
2022-08-24 04:49:11 +0000

    update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range (100):
     if i < 10:
         print("0{}".format(i), end=", ")
     elif i == 99:
-        print("{}".format(i), end="")
+        print(i)
     else:
         print("{}".format(i), end=", ")
