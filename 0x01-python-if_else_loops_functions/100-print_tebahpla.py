875791c1a8d63c858a284eb09bac199041cb2802
Samir Idris <samzidris11@gmail.com>
2022-08-25 03:42:59 +0000

    update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)))
+    print("{}".format(chr(c - i)), end="")
     if i == 0:
         i = 32
     else:
