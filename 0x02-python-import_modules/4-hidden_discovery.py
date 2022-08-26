d943a0cb914c897faf7839e140a98316a37c543d
Samir Idris <samzidris11@gmail.com>
2022-08-26 03:26:27 +0000

    update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ "__main__":
     import hidden_4
+
     names = dir(hidden_4)
-    new = []
-    for i in range(len(names)):
-        if names[i][:2] == __:
-            continue
-        else:
-            new += names[i]
-        new.sort()
-    for j in range(len(new)):
-        print(new[j])
+    for name in names:
+        if name[:2] != __:
+            print(i)
