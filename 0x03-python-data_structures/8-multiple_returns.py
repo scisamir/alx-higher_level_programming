40bbe07f2222fdb36aed019403b55638938f8e94
Samir Idris <samzidris11@gmail.com>
2022-08-29 15:36:52 +0000

    update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
-    return ((length, first_char))def multiple_returns(sentence):
+    if sentence == ""
+        first_char = None
+
     length = len(sentence)
     first_char = sentence[0]
     return ((length, first_char))
