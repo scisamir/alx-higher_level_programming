68da9d625bf3df7e919379b30dc1393a689d011d
Samir Idris <samzidris11@gmail.com>
2022-08-29 15:33:57 +0000

    create 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
+    return ((length, first_char))def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
+    return ((length, first_char))
