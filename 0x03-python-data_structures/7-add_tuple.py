3e062d9ef879d4d00e0a84d4b390c84088387967
Samir Idris <samzidris11@gmail.com>
2022-08-30 20:01:30 +0000

    create 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a += 0,
+    while len(tuple_b) < 2:
+        tuple_b += 0,
+
+    new_tup = ()
+    new_tup += ((tuple_a[0] + tuple_b[0]),)
+    new_tup += ((tuple_a[1] + tuple_b[1]),)
+    return new_tup
