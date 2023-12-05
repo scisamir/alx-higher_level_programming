1d444116841fe0035da5b30736f7c904989f7758
Samir Idris <samzidris11@gmail.com>
2023-12-05 12:06:38 -0800

    python network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,13 +3,17 @@
 My GitHub!
 """
 import requests
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
 if __name__ == "__main__":
+    usrn = argv[1]
+    pswd = argv[2]
+
     r = requests.get(
             'https://api.github.com/user',
-            auth=('scisamir', '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')
+            auth=HTTPBasicAuth(usrn, pswd)
         )
 
     print(r.json().get('id'))
