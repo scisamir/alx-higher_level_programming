e010d44ad826555c3e0bdbf615d88096e69654d2
Samir Idris <samzidris11@gmail.com>
2023-12-20 11:51:57 -0800

    filter states - mysqldb

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
             db=argv[3]
             )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+            "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+            )
     rows = cur.fetchall()
 
     for row in rows:
