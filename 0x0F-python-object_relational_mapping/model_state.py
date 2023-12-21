33b3ebe03ec66eaa97620362052a2db04c47c41f
Samir Idris <samzidris11@gmail.com>
2023-12-21 05:08:46 -0800

    first state model - sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Class State """
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
